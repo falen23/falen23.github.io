7e529d693a068ec3a14f3e936d997e08b319afd4
falen23 <byzad252@gmail.com>
2023-12-25 12:02:32 +0700

    portofolio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // toggel 
 const navSlide = () => {
-    const burger = document.querySelector(".burger")
-    const navLists = document.querySelector("nav")
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
 
     burger.addEventListener("click", () => {
         navLists.classList.toggle("nav-active");
